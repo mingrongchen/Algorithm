039e1d7731b1a7c4624592d2423a605264f1fb19
mingrongchen <825296313@qq.com>
2021-02-24 19:05:32 +0800

    1、初始化CLoin库。

---
+++ cmake-build-debug/CMakeFiles/algorithm_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm_c.dir/main.c.obj"
+  "algorithm_c.pdb"
+  "algorithm_c.exe"
+  "algorithm_c.exe.manifest"
+  "libalgorithm_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algorithm_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
